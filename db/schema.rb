e67da2518ba5b57d1b919a460a96a1fce08d63b9
Wojciech Winnicki <wbwinnicki@gmail.com>
2017-12-16 20:50:50 +0100

    Integrated database relationship between topics and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216111034) do
+ActiveRecord::Schema.define(version: 20171216194103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 20171216111034) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -54,4 +56,11 @@ ActiveRecord::Schema.define(version: 20171216111034) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "topics"
 end
