1f90e683b52e191f9580d0bbfd04ce9d253f2c42
Wojciech Winnicki <wbwinnicki@gmail.com>
2017-12-16 17:35:03 +0100

    Implemented custom action for updating status for posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  resources :posts
+  resources :posts do
+    member do
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
