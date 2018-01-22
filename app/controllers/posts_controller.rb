f7493589a0cf9d7f54c3844c7067dad0b55623cc
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-01-22 21:24:40 +0100

    Implemented he blog widget for topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:toggle_status, :update, :create, :destroy]
   access all: [:show, :index],
          user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]},
          site_admin: :all
@@ -98,5 +99,9 @@ class PostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body, :topic_id)
+    end
+
+    def set_sidebar_topics
+      @sidebar_topics = Topic.with_blogs
     end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'post'
   def index
-    @topics = Topic.all
+    @topics = Topic.with_blogs
   end
 
   def show
@@ -11,5 +12,10 @@ class TopicsController < ApplicationController
     else
       @posts = @topic.posts.published.recent.page(params[:page]).per(5)
     end
+  end
+
+  private
+  def set_sidebar_topics
+    @sidebar_topics = Topic.with_blogs
   end
 end
