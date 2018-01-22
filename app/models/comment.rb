5192e40b544b4cbd13db7044dd7bb8e03c127bb6
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-01-22 13:29:35 +0100

    Created comment broadcast, configured the wobsocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :content, presence: true, length: { minimum: 5, maximum: 240 }
+
+  after_create_commit { CommentBroadcastJob.perform_later(self) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :comments
   devise_for :users, path: '',
               path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   root to: 'pages#home'
@@ -21,5 +20,5 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
